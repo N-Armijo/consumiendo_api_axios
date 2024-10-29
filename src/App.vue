bd83603a1ba468a9b084370ee2315530238ba888
narmijo-tj <toffee.jaguargmail.com>
2024-10-29 18:46:05 -0300

    Consumiendo api con axios

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import {ref, onMounted} from 'vue';
+  import axios from 'axios';
+  const data = ref ([]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://randomuser.me/api/?results=200');
+      console.log(response);
+      data.value = response.data.results;
+    } catch (error) {
+      console.error(`Error: ${error}`);
+    }
+  };
+
+  onMounted( () => {
+    fetchData();
+  });
+
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="item in data">
+        {{ item.name.first }} {{ item.name.last }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
